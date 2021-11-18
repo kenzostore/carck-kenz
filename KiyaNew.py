e1a783491936c939abaf068cc3ef1171faf50d58
Aang Ardiansyah-XD <92802033+AngCyber@users.noreply.github.com>
2021-11-19 04:16:04 +0700

    Add files via upload

--- KiyaNew.py
+++ KiyaNew.py
@@ -658,15 +658,16 @@ class __crack__:
         else:
             print '\n %s[%s×%s] y/t GOBLOK!!'%(N,M,N);time.sleep(2);moch_yayan()
         return
-
+        
     def __api__(self, user, __yan__):
         global ok,cp,loop
         rm = random.choice(["\033[1;91m","\033[1;92m","\033[1;93m","\033[1;94m","\033[1;95m","\033[1;96m","\033[1;97m"])
         sys.stdout.write('\r [%s#%s][Retakan] %s/%s [OK-:%s][CP-:%s] '%(str(loping),len(mi),len(ok),len(cp))),
-        c_bff_ -= 1
-			else:
-				pass
-        
+        sys.stdout.flush()
+        for pw in __yan__:
+            pw = pw.lower()
+            try: os.mkdir('results')
+            except: pass 
             try:
             	_kontol = open('YNTKTS.txt', 'r').read()
             except (KeyError, IOError):
